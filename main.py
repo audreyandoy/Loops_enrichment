688fd3a6cc0225d9a59f5ceee3bfc48703f75fce
Audrey Andoy <audreylandoy@gmail.com>
2020-09-19 00:27:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# LOOPS
+
+# for loops, foreach loops, while loops
+
+for i in range(5):
+    print("waffles", i)
+
+print()
+
+grocery = ["apples", "pears", "bananas"]
+
+print(grocery)
+print(grocery[0])
+
+print()
+
+for i in grocery:
+    print(i)
+
+
+print()
+
+num = 0
+
+while num < 5:
+    num += 1
+    print(num)
